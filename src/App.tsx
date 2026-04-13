b5b73b82ca78f0bd003a425b070b7ed4c2ff7e76
zxchargedoffc <hahahahaialpa@mail.ru>
2026-04-13 22:29:07 +0500

    Fix mobile anchor navigation: App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
+import { useEffect } from 'react';
 import { BenefitsSection } from './components/BenefitsSection';
 import { Footer } from './components/Footer';
 import { FaqSection } from './components/FaqSection';
 import { Header } from './components/Header';
 import { HeroSection } from './components/HeroSection';
 import { PricingSection } from './components/PricingSection';
+import { scrollToCurrentHash } from './lib/hashScroll';
 
 function App() {
+  useEffect(() => {
+    scrollToCurrentHash('auto');
+
+    const handleHashChange = () => {
+      scrollToCurrentHash('smooth');
+    };
+
+    window.addEventListener('hashchange', handleHashChange);
+
+    return () => {
+      window.removeEventListener('hashchange', handleHashChange);
+    };
+  }, []);
+
   return (
     <main className="site-shell">
       <div className="ambient ambient-one" />

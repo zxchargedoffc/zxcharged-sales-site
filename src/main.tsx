fef4c609c87043df330fe1a188432f30a7f87baf
zxchargedoffc <hahahahaialpa@mail.ru>
2026-04-13 21:55:51 +0500

    Fix mobile landing render fallback: src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
